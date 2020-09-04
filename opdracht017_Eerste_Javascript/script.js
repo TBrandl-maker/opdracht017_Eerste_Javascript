eb3ccd35540dac50b371cfcfb614b974edd50202
TBrandl <58363852+TBrandl-maker@users.noreply.github.com>
2020-09-04 13:14:28 +0200

    Add files via upload

---
+++ opdracht017_Eerste_Javascript/script.js
@@ -0,0 +1,19 @@
+console.log("Hello Winc Academy")
+
+let name = 'Tatjana';
+console.log(name);
+
+console.log(4 + 4);
+console.log("4" + "4");
+console.log(4 * 3);
+console.log(50 - 10);
+console.log(50 * 5);
+console.log(75 / 10);
+console.log(25 % 3)
+console.log("3" + "5")
+
+let age = 35;
+console.log(typeof age);
+
+let ageAsString = 'vijfendertig';
+console.log(typeof ageAsString);
